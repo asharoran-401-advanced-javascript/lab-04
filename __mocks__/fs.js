1ba730da4025db4c40bb6227174b954f6a5f7eeb
asharaloran96 <asharoran96@gmail.com>
2020-01-23 07:26:21 +0200

    eslint issues

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,24 +1,32 @@
+/* eslint-disable no-undef */
 'use strict';
 
+// eslint-disable-next-line no-undef
 module.exports = exports = {};
 
 var fileContents = '';
 
+// eslint-disable-next-line no-undef
 exports.readFile = (file, cb) => {
   if (file.match(/bad/i)) {
     cb('Invalid File');
   }
   else {
+    // eslint-disable-next-line no-undefined
+    // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-undefined
     cb(undefined, Buffer.from(fileContents));
   }
 };
 
+// eslint-disable-next-line no-undef
 exports.writeFile = (file, buffer, cb) => {
   if (file.match(/bad/i)) {
     cb('Invalid File');
   }
   else {
     fileContents = buffer;
+    // eslint-disable-next-line no-undefined
     cb(undefined, true);
   }
 };
